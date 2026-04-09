8024f35e65f30a73f43b32130e3974de2d06d162
rhikipratama <rhiki600@gmail.com>
2026-04-09 20:23:09 +0700

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coffee Shop Bayang</title>
+
+    <!-- SWIPER -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+    <!-- Font Awesome CDN Link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- Custom CSS File Link  -->
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+    <!-- HEADER -->
+    <header class="header">
+        <div id="menu-btn" class="fas fa-bars"></div>
+
+        <a href="#" class="logo">Bayang Healing Space <i class="fas fa-mug-hot"></i></a>
+
+        <nav class="navbar">
+            <a href="#home">home</a>
+            <a href="#about">about</a>
+            <a href="#menu">menu</a>
+            <a href="#contact">contact</a>
+        </nav>
+
+        <a href="#book" class="btn">lihat menu</a>
+    </header>
+
+    <!-- HOME -->
+    <section class="home" id="home">
+        <div class="row">
+            <div class="content">
+                <h3>awali hari dengan secangkir kopi</h3>
+                <a href="https://drive.google.com/drive/mobile/folders/13V0rniCfiPsMlj3YMVyKYlcxn-yFCB9p?fbclid=PAY2xjawG7XexleHRuA2FlbQIxMAABpqwUW14n01ijW5UWJ8mVgXjlx_Nb7bGzbq7_BrmI5KCBPtFMgqxvVCFQ0A_aem_8h7NZVpOv0CVWt_lc091Uw" class="btn">pesan sekarang</a>
+            </div>
+
+            <div class="image">
+                <img src="../img/home-img-1.png" class="main-home-image" alt="">
+            </div>
+        </div>
+
+        <div class="image-slider">
+            <img src="../img/home-img-1.png" alt="">
+            <img src="../img/home-img-2.png" alt="">
+            <img src="../img/home-img-3.png" alt="">
+        </div>
+    </section>
+
+    <!-- ABOUT -->
+    <section class="about" id="about">
+        <h1 class="heading">about us <span>why choose us</span></h1>
+
+        <div class="row">
+            <div class="image">
+                <img src="../img/about-img.jpeg" alt="">
+            </div>
+
+            <div class="content">
+                <h3 class="title">Bayang Healing Space</h3>
+                <p>Coffee shop di Bandung yang mengusung tema industrial modern dengan sentuhan elemen kayu dan tanaman hijau, menciptakan suasana nyaman sekaligus estetik. Dan juga mengadopsi gaya vintage, dengan furnitur klasik dan dekorasi unik, yang cocok untuk berfoto...</p>
+                <a href="#" class="btn">read more</a>
+                <div class="icons-container">
+                    <div class="icons">
+                        <img src="../img/about-icon-1.png" alt="">
+                        <h3>quality coffee</h3>
+                    </div>
+                    <div class="icons">
+                        <img src="../img/about-icon-2.png" alt="">
+                        <h3>our branches</h3>
+                    </div>
+                    <div class="icons">
+                        <img src="../img/about-icon-3.png" alt="">
+                        <h3>free delivery</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- MENU -->
+    <section class="menu" id="menu">
+        <h1 class="heading">our menu <span>popular menu</span></h1>
+
+        <div class="box-container">
+            <a href="#" class="box">
+                <img src="../img/menu-1.png" alt="">
+                <div class="content">
+                    <h3>Cappucino</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur, sed.</p>
+                    <span>Rp.25,000</span>
+                </div>
+            </a>
+
+            <a href="#" class="box">
+                <img src="../img/menu-2.png" alt="">
+                <div class="content">
+                    <h3>Cafe Latte</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel, fugit.</p>
+                    <span>Rp.25,000</span>
+                </div>
+            </a>
+
+            <a href="#" class="box">
+                <img src="../img/menu-3.png" alt="">
+                <div class="content">
+                    <h3>Red Wine</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, recusandae.</p>
+                    <span>Rp.25,000</span>
+                </div>
+            </a>
+
+            <a href="#" class="box">
+                <img src="../img/menu-4.png" alt="">
+                <div class="content">
+                    <h3>Black Amplifier</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, quas.</p>
+                    <span>Rp.25,000</span>
+                </div>
+            </a>
+
+            <a href="#" class="box">
+                <img src="../img/menu-5.png" alt="">
+                <div class="content">
+                    <h3>Kopi Bayang</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, vitae.</p>
+                    <span>Rp.25,000</span>
+                </div>
+            </a>
+
+            <a href="#" class="box">
+                <img src="../img/menu-6.png" alt="">
+                <div class="content">
+                    <h3>Kopeach</h3>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde, expedita!</p>
+                    <span>Rp.25,000</span>
+                </div>
+            </a>
+        </div>
+    </section>
+
+    <!-- CONTACT -->
+    <section class="contact" id="contact">
+        <h1 class="heading">contact us <span>how to contact us</span></h1>
+
+        <div class="swiper contact-slider">
+            <a href="https://wa.me/082372724448" class="swiper-wrapper">
+                <div class="swiper-slide box">
+                    <i class="fas fa-quote-left"></i>
+                    <i class="fas fa-quote-right"></i>
+                    <i class="fab fa-whatsapp-square"></i>
+                    <h3>whatsapp</h3>
+                    <span>--- hubungi kami ---</span>
+                </div>            
+            </a>
+            <div class="contact-pagination"></div>
+        </div>
+
+        <div class="swiper contact-slider">
+            <a href="https://www.instagram.com/rhikiprtmaa" class="swiper-wrapper">
+                <div class="swiper-slide box">
+                    <i class="fas fa-quote-left"></i>
+                    <i class="fas fa-quote-right"></i>
+                    <i class="fab fa-instagram-square"></i>
+                    <h3>instagram</h3>
+                    <span>--- sosial media kami ---</span>
+                </div>            
+            </a>
+            <div class="contact-pagination"></div>
+        </div>
+
+        <div class="swiper contact-slider">
+            <a href="#" class="swiper-wrapper">
+                <div class="swiper-slide box">
+                    <i class="fas fa-quote-left"></i>
+                    <i class="fas fa-quote-right"></i>
+                    <i class="fas fa-map-marker-alt"></i>
+                    <h3>maps</h3>
+                    <span>--- alamat kami ---</span>
+                </div>            
+            </a>
+            <div class="contact-pagination"></div>
+        </div>
+    </section>
+
+    <!-- SWIPER -->
+    <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+    <!-- Custom JS File Link  -->
+    <script src="../js/script.js"></script>
+</body>
+
+</html>
